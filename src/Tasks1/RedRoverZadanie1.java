b6cef531a4fb564d6a2693aabe9bd408155bea9d
Ruslan <Rahimbayr@yandex.ru>
2025-08-13 23:43:29 +0400

    RedRover

---
+++ src/Tasks1/RedRoverZadanie1.java
@@ -0,0 +1,13 @@
+package Tasks1;
+
+public class RedRoverZadanie1 {
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println(2);
+        System.out.println(1);
+        System.out.println("Поехали!!!");
+    }
+
+}
+
+
---
+++ src/Tasks1/Zadanie3.java
@@ -0,0 +1,12 @@
+package Tasks1;
+
+public class Zadanie3 {
+    public static void main(String[] args) {
+        System.out.println("\uD83D\uDE03");
+
+
+
+    }
+
+
+}
